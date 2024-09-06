f4068de4c8a14db874d924ee62adf19420fe408b
micsthepick <11528421+micsthepick@users.noreply.github.com>
2024-09-06 12:49:27 +1000

    ignore very simple regexes

--- watched_keywords_fuzzer.py
+++ watched_keywords_fuzzer.py
@@ -15,16 +15,22 @@ load_blacklists()
 
 KWDS = GlobalVars.watched_keywords.keys()
 
-REGEXES = [regex_compile_no_cache(kw, regex.UNICODE, city=city_list, ignore_unused=True) for kw in KWDS]
+print(f'loaded {len(KWDS)} KWDS')
+
+REGEXES = [regex_compile_no_cache(kw, regex.UNICODE, city=city_list, ignore_unused=True) for kw in KWDS if len(set('<>[]()') & set(kw))]
+
+
+print(f'rejected {len(KWDS)-len(REGEXES)} KWDS (too simple) and left with {len(REGEXES)}')
 
 @atheris.instrument_func
 def TestAllWatchedKeywords(data: bytes):
     # Check each REGEX one by one, recording how long data takes
     # find out which took the longest, and print it
 
-    # first make sure that we are using valid unicode.
+    fdp = atheris.FuzzedDataProvider(data)
+
     try:
-        string = str(data, encoding='utf-8')
+        string = fdp.ConsumeUnicode(len(data))
     except UnicodeDecodeError:
         return
 
@@ -44,8 +50,24 @@ def TestAllWatchedKeywords(data: bytes):
         if elapsed_time > longest_time:
             longest_time = elapsed_time
             slowest_regex = r
-    
-    if longest_time > 0.25:
+        if elapsed_time > 0.0001:
+            pass
+        elif elapsed_time > 0.0003:
+            pass
+        elif elapsed_time > 0.001:
+            pass
+        elif elapsed_time > 0.003:
+            pass
+        elif elapsed_time > 0.01:
+            pass
+        elif elapsed_time > 0.03:
+            pass
+        elif elapsed_time > 0.1:
+            pass
+        elif elapsed_time > 0.3:
+            pass
+
+    if longest_time > 1:
         # Print the slowest regex and the time it took if it takes a considerable time
         print(f"\nSlowest regex: {slowest_regex.pattern}, Time taken: {longest_time:.4f} seconds")
         raise RuntimeError('Boom')
